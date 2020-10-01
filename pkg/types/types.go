95cc7b02a915d5283e37d1d109d387b1dd8b0a22
Khushbakht Mahkamov <khushbakhtmahkamov@gmail.com>
2020-10-01 15:25:29 +0500

    add funcs

---
+++ pkg/types/types.go
@@ -0,0 +1,66 @@
+package types
+
+type Money int64
+
+type PaymentCategory string
+
+type PaymentStatus string
+
+const (
+	PaymentStatusOk PaymentStatus = "OK"
+	PaymentStatusFail PaymentStatus = "FAIL"
+	PaymentStatusInProgress PaymentStatus = "INPROGRESS"
+)
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+type Category string
+
+type Status string
+
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
+type PAN string
+
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+	MinBalance Money
+}
+
+type Payment struct {
+	ID     string
+	AccountID int64
+	Amount Money
+	Category PaymentCategory
+	Status   PaymentStatus
+}
+
+type Phone string
+
+type Account struct {
+	ID int64
+	Phone Phone
+	Balance Money
+}
+
+type PaymentSource struct {
+	Type    string //'card'
+	Number  string //
+	Balance Money
+}
---
+++ pkg/wallet/service.go
@@ -0,0 +1,93 @@
+package wallet
+
+import (
+	"errors"
+	"github.com/khushbakhtmahkamov/wallet/pkg/types"
+	
+)
+
+
+type Error string
+
+func (e Error) Error() string {
+	return string(e)
+}
+
+var ErrPhoneRegistered = errors.New("Phone already registered")
+var ErrAmountMustBePositive = errors.New("amount must be greater than 0")
+var ErrAccountNotFound = errors.New("account not found")
+
+type Service struct{
+	nextAccountID int64
+	accounts []*types.Account
+	payments []*types.Payment
+}
+
+func RegisterAccount(service *Service,phone types.Phone)  {
+	for _, account := range service.accounts {
+		if account.Phone == phone {
+			return
+		}
+	}
+
+	service.nextAccountID++
+	service.accounts=append(service.accounts,&types.Account{
+		ID: service.nextAccountID,
+		Phone: phone,
+		Balance: 0,
+	})
+}
+
+func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error)  {
+	for _, account := range s.accounts {
+		if account.Phone == phone {
+			return nil, ErrPhoneRegistered
+		}
+	}
+
+	s.nextAccountID++
+	account:= &types.Account{
+		ID: s.nextAccountID,
+		Phone: phone,
+		Balance: 0,
+	}
+	s.accounts=append(s.accounts,account)
+	return account,nil
+}
+
+func (s *Service) Deposit(accountID int64,amount types.Money) error {
+	if amount<=0 {
+		return ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return ErrAccountNotFound
+	}
+
+	account.Balance += amount
+	return nil
+}
+
+func (s *Service) FindAccountByID(accountID int64) (*types.Account, error)  {
+	
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+	return account,nil
+}
